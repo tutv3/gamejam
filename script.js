caee131198e90faa4b695b36e212b20856f69473
tutv3 <tutv3@smartosc.com>
2021-12-27 18:00:08 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,362 @@
+const dataJson = {
+  videos: ["https://assets.gamejam.co/platform/gamejam.mp4"],
+  bigFeaturedJam: {
+    updateAt: 1603270278228,
+    createAt: 1602585156481,
+    id: 3,
+    startTime: 1603356600000,
+    endTime: 1603457400000,
+    resultTime: 1603543800000,
+    name: "gamejam 1",
+    slug: "the-1",
+    status: 1,
+    description: "ouhfiudhfiudsfhiu iuheiuwfh;eiufhew;fuihew;fe ;u h;uh",
+    details: "item-ed",
+    thumbnailImageUrl: "https://picsum.photos/600/300",
+    priority: 0,
+    coverImageUrl: "https://picsum.photos/1200/600",
+    hostProfiles: [
+      {
+        username: "admin",
+        avatarUrl: "https://picsum.photos/300/300",
+      },
+    ],
+    submissionCount: 1000,
+    joinedCount: 100,
+  },
+  jams: [
+    {
+      updateAt: 1603270278228,
+      createAt: 1602585156481,
+      id: 3,
+      startTime: 1603356600000,
+      endTime: 1603457400000,
+      resultTime: 1603543800000,
+      name: "gamejam 1",
+      slug: "the-1",
+      status: 1,
+      description: "ouhfiudhfiudsfhiu iuheiuwfh;eiufhew;fuihew;fe ;u h;uh",
+      details: "item-ed",
+      thumbnailImageUrl: "https://picsum.photos/600/300",
+      priority: 0,
+      coverImageUrl: "https://picsum.photos/1200/600",
+      hostProfiles: [
+        {
+          username: "gamejam",
+          avatarUrl: "https://picsum.photos/300/300",
+        },
+      ],
+      submissionCount: 1000,
+      joinedCount: 100,
+    },
+    {
+      updateAt: 1603270278228,
+      createAt: 1602585156481,
+      id: 4,
+      startTime: 1603356600000,
+      endTime: 1603457400000,
+      resultTime: 1603543800000,
+      name: "gamejam 2",
+      slug: "the-2",
+      status: 1,
+      description: "this is description",
+      details: "item-ed",
+      thumbnailImageUrl: "https://picsum.photos/600/300",
+      priority: 0,
+      coverImageUrl: "https://picsum.photos/1200/600",
+      hostProfiles: [
+        {
+          username: "admin",
+          avatarUrl: "https://picsum.photos/300/300",
+        },
+      ],
+      submissionCount: 1000,
+      joinedCount: 100,
+    },
+    {
+      updateAt: 1603270278228,
+      createAt: 1602585156481,
+      id: 4,
+      startTime: 1603356600000,
+      endTime: 1603457400000,
+      resultTime: 1603543800000,
+      name: "gamejam 2",
+      slug: "the-2",
+      status: 7,
+      description: "this is description",
+      details: "item-ed",
+      thumbnailImageUrl: "https://picsum.photos/600/300",
+      priority: 0,
+      coverImageUrl: "https://picsum.photos/1200/600",
+      hostProfiles: [
+        {
+          username: "admin",
+          avatarUrl: "https://picsum.photos/300/300",
+        },
+      ],
+      submissionCount: 1000,
+      joinedCount: 100,
+    },
+    {
+      updateAt: 1603270278228,
+      createAt: 1602585156481,
+      id: 4,
+      startTime: 1603356600000,
+      endTime: 1603457400000,
+      resultTime: 1603543800000,
+      name: "gamejam 2",
+      slug: "the-2",
+      status: 6,
+      description: "this is description",
+      details: "item-ed",
+      thumbnailImageUrl: "https://picsum.photos/600/300",
+      priority: 0,
+      coverImageUrl: "https://picsum.photos/1200/600",
+      hostProfiles: [
+        {
+          username: "admin",
+          avatarUrl: "https://picsum.photos/300/300",
+        },
+      ],
+      submissionCount: 1000,
+      joinedCount: 100,
+    },
+    {
+      updateAt: 1603270278228,
+      createAt: 1602585156481,
+      id: 5,
+      startTime: 1603356600000,
+      endTime: 1603457400000,
+      resultTime: 1603543800000,
+      name: "gamejam 2",
+      slug: "the-2",
+      status: 1,
+      description: "this is description",
+      details: "item-ed",
+      thumbnailImageUrl: "https://picsum.photos/600/300",
+      priority: 0,
+      coverImageUrl: "https://picsum.photos/1200/600",
+      hostProfiles: [
+        {
+          username: "admin",
+          avatarUrl: "https://picsum.photos/300/300",
+        },
+      ],
+      submissionCount: 1000,
+      joinedCount: 100,
+    },
+  ],
+  games: [
+    {
+      code: 200,
+      message: "OK!",
+      data: [
+        {
+          updateAt: 1602661566862,
+          createAt: 1602661566862,
+          id: 23,
+          userId: 1,
+          jamId: 6,
+          name: "Game1",
+          description: ";sdaselgk wkehwfwef descriptionf",
+          details: "item-ed",
+          thumbnailImageUrl: null,
+          status: 1,
+          creatorProfile: {
+            username: "admin",
+            avatarUrl: "",
+            socialUrls: null,
+          },
+          jamName: "this is second jam",
+          jamSlug: "this-is-second-jam",
+          reviewCount: 0,
+          downloadCount: 0,
+          screenshots: [],
+          files: [
+            {
+              updateAt: 1602661566895,
+              createAt: 1602661566895,
+              id: 24,
+              gameId: 23,
+              type: 1,
+              meta: null,
+              url: "",
+            },
+          ],
+          rate: 4.0,
+        },
+        {
+          updateAt: 1603163775216,
+          createAt: 1603163775216,
+          id: 121,
+          userId: 2,
+          jamId: 116,
+          name: "This is the game game game game",
+          description: "",
+          details: "item-ed",
+          thumbnailImageUrl: "https://picsum.photos/600/300",
+          status: 1,
+          creatorProfile: {
+            username: "admin",
+            avatarUrl: "https://picsum.photos/300/300",
+            socialUrls: null,
+          },
+          jamName: "Testing gamejam m",
+          jamSlug: "testing-gamejam-m",
+          reviewCount: 0,
+          downloadCount: 0,
+          screenshots: [],
+          files: [
+            {
+              updateAt: 1603163484230,
+              createAt: 1603163484230,
+              id: 124,
+              gameId: 121,
+              type: 1,
+              meta: '{"platform":"Windows","description":"pijeroijewiojeoijoie"}',
+              url: "",
+            },
+          ],
+          rate: 4.0,
+        },
+        {
+          updateAt: 1603163775216,
+          createAt: 1603163775216,
+          id: 121,
+          userId: 2,
+          jamId: 116,
+          name: "This is the game game game game",
+          description: "",
+          details: "item-ed",
+          thumbnailImageUrl: "https://picsum.photos/600/300",
+          status: 1,
+          creatorProfile: {
+            username: "admin",
+            avatarUrl: "https://picsum.photos/300/300",
+            socialUrls: null,
+          },
+          jamName: "Testing gamejam m",
+          jamSlug: "testing-gamejam-m",
+          reviewCount: 0,
+          downloadCount: 0,
+          screenshots: [],
+          files: [
+            {
+              updateAt: 1603163484230,
+              createAt: 1603163484230,
+              id: 124,
+              gameId: 121,
+              type: 1,
+              meta: '{"platform":"Windows","description":"pijeroijewiojeoijoie"}',
+              url: "",
+            },
+          ],
+          rate: 4.0,
+        },
+      ],
+    },
+  ],
+};
+
+const featureJamsListEl = document.querySelector(".feature-jams__list");
+const featureGamesListEl = document.querySelector(".feature-games__list");
+const showFullJamsEl = document.getElementById("show-full__jams");
+const showFullGamesEl = document.getElementById("show-full__games");
+const showcaseEl = document.getElementById("showcase");
+
+const { jams, games, bigFeaturedJam } = dataJson;
+
+function renderJamsList(isShowFull = false) {
+  if (isShowFull) {
+    jams.slice(3, jams.length).forEach((item) => renderJamsItem(item));
+    hideShowFullJamsBtn();
+  } else {
+    jams.slice(0, 3).forEach((item) => renderJamsItem(item));
+  }
+}
+
+function renderJamsItem(item) {
+  const jamsItemEl = document.createElement("div");
+
+  jamsItemEl.className = "feature-jams__item";
+
+  const wasteTime = (item.endTime - item.startTime) / 1000;
+
+  const days = Math.floor(wasteTime / 86400);
+
+  const hours = Math.floor((wasteTime - days * 86400) / 3600);
+
+  const percent = Math.ceil((item.joinedCount / item.submissionCount) * 100);
+
+  jamsItemEl.innerHTML = `<div class="feature-jams__media"><img src="${
+    item.thumbnailImageUrl
+  }" alt="${item.description}"/></div>
+                        <div class="feature-jams__content">
+                          <h4 class="feature-jams__title">${item.name}</h4>
+                          <h5 class="feature-jams__subtitle">Hosted by ${
+                            item.hostProfiles?.[0]?.username
+                          }</h5>
+                          <p class="feature-jams__time">Start in ${days} days, ${hours} hours</p>
+                          <div class="feature-jams__progress-container">
+                            <div class="feature-jams__progress" style="width: ${
+                              percent * 3
+                            }%" ></div>
+                          </div>
+                          <div class="feature-jams__join">
+                            <span class="me-4">${item.joinedCount} Joined</span>
+                            <span>${item.submissionCount} Submissions</span>
+                          </div>
+                        </div>`;
+  featureJamsListEl.appendChild(jamsItemEl);
+}
+
+function renderGamesList(isShowFull = false) {
+  if (isShowFull) {
+    games[0].data
+      .slice(3, games[0].data.length)
+      .forEach((item) => renderGamesItem(item));
+    hideShowFullGamesBtn();
+  } else {
+    games[0].data.slice(0, 3).forEach((item) => renderGamesItem(item));
+  }
+}
+
+function renderGamesItem(item) {
+  const gamesItemEl = document.createElement("div");
+
+  gamesItemEl.className = "feature-games__item";
+
+  gamesItemEl.innerHTML = `<div class="feature-games__media"><img src="${
+    item.thumbnailImageUrl || "https://picsum.photos/600/300"
+  }" alt="${item.name}"/></div>
+                        <div class="feature-games__content">
+                          <div class="feature-games__content-top">
+                            <h4 class="feature-games__title">${item.name}</h4>
+                            <strong class="feature-games__price">$3.99</strong>
+                          </div>
+                          <h5 class="feature-games__subtitle">#Action</h5>
+                          <p class="feature-games__time">${item.details}</p>
+                        </div>`;
+  featureGamesListEl.appendChild(gamesItemEl);
+}
+
+function hideShowFullJamsBtn() {
+  showFullJamsEl.classList.add("d-none");
+}
+
+function hideShowFullGamesBtn() {
+  showFullGamesEl.classList.add("d-none");
+}
+
+//  start application
+function initApp() {
+  renderJamsList();
+  renderGamesList();
+}
+
+initApp();
+
+// events
+
+showFullJamsEl.addEventListener("click", () => renderJamsList(true));
+showFullGamesEl.addEventListener("click", () => renderGamesList(true));
